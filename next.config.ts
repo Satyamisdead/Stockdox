64504afbb753eb475009df3a641f6b53e0d59042
Joona Linna <cosmosecuria@gmail.com>
2025-06-15 18:25:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         hostname: 's3-symbol-logo.tradingview.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static2.finnhub.io',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
