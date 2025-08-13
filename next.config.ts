310a156f1fb85ad683f37bba39892feddab5bc25
Joona Linna <cosmosecuria@gmail.com>
2025-08-13 08:52:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -58,6 +58,12 @@ const nextConfig: NextConfig = {
         hostname: 'assets.coingecko.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
